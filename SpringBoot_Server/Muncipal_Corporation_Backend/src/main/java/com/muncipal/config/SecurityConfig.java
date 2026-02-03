25353929bac3a63113a7845274a8d21ca5057c90
Shubham Kadam <kadamshubham9744@gmail.com>
2026-02-03 17:51:12 +0530

    Cors isuues resolved

--- SpringBoot_Server/Muncipal_Corporation_Backend/src/main/java/com/muncipal/config/SecurityConfig.java
+++ SpringBoot_Server/Muncipal_Corporation_Backend/src/main/java/com/muncipal/config/SecurityConfig.java
@@ -4,12 +4,16 @@ import com.muncipal.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.*;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,42 +25,86 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                // Disable CSRF (Stateless APIs)
+                // CORS
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
                 .csrf(csrf -> csrf.disable())
 
-                // Stateless Session
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
 
-                // Authorization Rules
                 .authorizeHttpRequests(auth -> auth
 
-                        // 1. PUBLIC ENDPOINTS (Open to everyone)
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                        // PUBLIC
                         .requestMatchers(
                                 "/user/login",
                                 "/user/register",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui.html"
-//                                "/**"
                         ).permitAll()
 
-                        .requestMatchers(
-                                "/admin/**"
-                        ).hasRole("ADMIN")
+                        // CITIZEN
+                        .requestMatchers(HttpMethod.POST, "/certificateController").hasRole("CITIZEN")
+                        .requestMatchers(HttpMethod.GET,  "/certificateController/birth/**").hasRole("CITIZEN")
+                        .requestMatchers(HttpMethod.GET,  "/certificateController/death/**").hasRole("CITIZEN")
 
-                        .requestMatchers(
-                                "/citizen/**"
-                        ).hasRole("CITIZEN")
+                        .requestMatchers(HttpMethod.GET,  "/properties/citizen/**").hasRole("CITIZEN")
+                        .requestMatchers(HttpMethod.GET,  "/properties/**").hasRole("CITIZEN")
+                        .requestMatchers(HttpMethod.PATCH,"/properties/taxUpdate/**").hasRole("CITIZEN")
+
+                        .requestMatchers(HttpMethod.POST, "/grievances").hasRole("CITIZEN")
+                        .requestMatchers(HttpMethod.GET,  "/grievances/**").hasRole("CITIZEN")
+
+                        .requestMatchers(HttpMethod.POST, "/payment/create-order").hasRole("CITIZEN")
+                        .requestMatchers(HttpMethod.POST, "/payment/verify").hasRole("CITIZEN")
+
+                        //  ADMIN
+                        .requestMatchers(HttpMethod.GET,   "/properties").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/properties/**").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET,   "/certificateController/birth").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,   "/certificateController/death").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/certificateController/**").hasRole("ADMIN")
+
+                        .requestMatchers(HttpMethod.GET,   "/grievances").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,   "/grievances/all").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/grievances/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
                 )
 
-                // Add JWT Filter
+                // JWT filter
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://69819a3bf8107372177ec984--quiet-klepon-b43f8a.netlify.app"
+        ));
+
+        config.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
     @Bean
